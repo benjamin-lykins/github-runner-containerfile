a893dbe58b76c415e3064925504fe1e779b4da29
Ben Lykins <benjamin.m.lykins@gmail.com>
2024-04-28 14:53:25 -0400

    troubleshooting

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@ REPOSITORY=$REPO
 ACCESS_TOKEN=$TOKEN
 
 # These are only used for debugging, you would not want to expose your token in a real-world scenario.
-# echo "REPO ${REPOSITORY}"
-# echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
+echo "REPO ${REPOSITORY}"
+echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
 
 REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
 
